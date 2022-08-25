3f401c146c2dcb6d61dd5f0c438febdd71ff137c
Jianxi Huang <64356206+BENIMALS9@users.noreply.github.com>
2022-08-25 10:15:29 +0800

    first version

---
+++ foodie map/js/map_color.js
@@ -0,0 +1,571 @@
+var gz_color = [
+    {
+        name: '花都区',
+        itemStyle: {
+            areaColor: '#75a0a7'
+        },
+        label: {
+            color: '#75a0a7',
+            show: false
+        }
+    },
+    {
+        name: '白云区',
+        itemStyle: {
+            areaColor: '#a9cbd0'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '天河区',
+        itemStyle: {
+            areaColor: '#eae8bc'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '海珠区',
+        itemStyle: {
+            areaColor: '#eae8bc'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '越秀区',
+        itemStyle: {
+            areaColor: '#eae8bc'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '荔湾区',
+        itemStyle: {
+            areaColor: '#eae8bc'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '番禺区',
+        itemStyle: {
+            areaColor: '#dab46b'
+        },
+        label: {
+            color: '#ebb489',
+            show: false
+        }
+    },
+    {
+        name: '黄埔区',
+        itemStyle: {
+            areaColor: '#a4c6a8'
+        },
+        label: {
+            color: '#a4c6a8',
+            show: false
+        }
+    },
+    {
+        name: '从化区',
+        itemStyle: {
+            areaColor: '#94c082'
+        },
+        label: {
+            color: '#94c082',
+            show: false
+        }
+    },
+    {
+        name: '增城区',
+        itemStyle: {
+            areaColor: '#76a675'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '南沙区',
+        itemStyle: {
+            areaColor: '#d68a76'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    }
+]
+
+var cd_color = [
+    {
+        name: '郫都区',
+        itemStyle: {
+            areaColor: '#9dca9b'
+        },
+        label: {
+            color: '#75a0a7',
+            show: false
+        }
+    },
+    {
+        name: '温江区',
+        itemStyle: {
+            areaColor: '#7bb1b6'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '双流区',
+        itemStyle: {
+            areaColor: '#a29ec3'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '新津区',
+        itemStyle: {
+            areaColor: '#7b84ac'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '金牛区',
+        itemStyle: {
+            areaColor: '#bad3a4'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '青羊区',
+        itemStyle: {
+            areaColor: '#a6c3c9'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '武侯区',
+        itemStyle: {
+            areaColor: '#cbbedb'
+        },
+        label: {
+            color: '#ebb489',
+            show: false
+        }
+    },
+    {
+        name: '新都区',
+        itemStyle: {
+            areaColor: '#89bca3'
+        },
+        label: {
+            color: '#a4c6a8',
+            show: false
+        }
+    },
+    {
+        name: '成华区',
+        itemStyle: {
+            areaColor: '#ede7b6'
+        },
+        label: {
+            color: '#94c082',
+            show: false
+        }
+    },
+    {
+        name: '锦江区',
+        itemStyle: {
+            areaColor: '#e0c690'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '青白江区',
+        itemStyle: {
+            areaColor: '#daad80'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '龙泉驿区',
+        itemStyle: {
+            areaColor: '#d1a369'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    }
+]
+
+var wh_color = [
+    {
+        name: '黄陂区',
+        itemStyle: {
+            areaColor: '#7fb5b8'
+        },
+        label: {
+            color: '#75a0a7',
+            show: false
+        }
+    },
+    {
+        name: '东西湖区',
+        itemStyle: {
+            areaColor: '#dcacb2'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '蔡甸区',
+        itemStyle: {
+            areaColor: '#ce8787'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '汉南区',
+        itemStyle: {
+            areaColor: '#ce8fa3'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '新洲区',
+        itemStyle: {
+            areaColor: '#256397'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '洪山区',
+        itemStyle: {
+            areaColor: '#a4cad5'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '江夏区',
+        itemStyle: {
+            areaColor: '#7c598d'
+        },
+        label: {
+            color: '#ebb489',
+            show: false
+        }
+    },
+    {
+        name: '汉阳区',
+        itemStyle: {
+            areaColor: '#3a9795'
+        },
+        label: {
+            color: '#a4c6a8',
+            show: false
+        }
+    },
+    {
+        name: '江岸区',
+        itemStyle: {
+            areaColor: '#8ebaad'
+        },
+        label: {
+            color: '#94c082',
+            show: false
+        }
+    },
+    {
+        name: '硚口区',
+        itemStyle: {
+            areaColor: '#8ebaad'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '江汉区',
+        itemStyle: {
+            areaColor: '#2d937a'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '武昌区',
+        itemStyle: {
+            areaColor: '#aeabca'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '青山区',
+        itemStyle: {
+            areaColor: '#464883'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    }
+]
+
+var cq_color = [
+    {
+        name: '大足区',
+        itemStyle: {
+            areaColor: '#d9b266'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '荣昌区',
+        itemStyle: {
+            areaColor: '#ce994f'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '合川区',
+        itemStyle: {
+            areaColor: '#deaf81'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '铜梁区',
+        itemStyle: {
+            areaColor: '#e2d199'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '永川区',
+        itemStyle: {
+            areaColor: '#d7ae68'
+        },
+        label: {
+            color: '#eae8bc',
+            show: false
+        }
+    },
+    {
+        name: '北碚区',
+        itemStyle: {
+            areaColor: '#7c598d'
+        },
+        label: {
+            color: '#ebb489',
+            show: false
+        }
+    },
+    {
+        name: '璧山区',
+        itemStyle: {
+            areaColor: '#7bb1a2'
+        },
+        label: {
+            color: '#a4c6a8',
+            show: false
+        }
+    },
+    {
+        name: '江津区',
+        itemStyle: {
+            areaColor: '#2d937a'
+        },
+        label: {
+            color: '#94c082',
+            show: false
+        }
+    },
+    {
+        name: '九龙坡区',
+        itemStyle: {
+            areaColor: '#3a9795'
+        },
+        label: {
+            color: '#ffffff',
+            show: false
+        }
+    },
+    {
+        name: '沙坪坝区',
+        itemStyle: {
+            areaColor: '#8ebaad'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '渝北区',
+        itemStyle: {
+            areaColor: '#8c89b1'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '江北区',
+        itemStyle: {
+            areaColor: '#aeabca'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '南岸区',
+        itemStyle: {
+            areaColor: '#CBC7EB'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '巴南区',
+        itemStyle: {
+            areaColor: '#a4cad5'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '渝中区',
+        itemStyle: {
+            areaColor: '#D9A8F0'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '綦江区',
+        itemStyle: {
+            areaColor: '#3a9795'
+        },
+        label: {
+            color: '#a4cad5',
+            show: false
+        }
+    },
+    {
+        name: '长寿区',
+        itemStyle: {
+            areaColor: '#464883'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '涪陵区',
+        itemStyle: {
+            areaColor: '#81BDEE'
+        },
+        label: {
+            color: '#246296',
+            show: false
+        }
+    },
+    {
+        name: '南川区',
+        itemStyle: {
+            areaColor: '#246296'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    },
+    {
+        name: '大渡口区',
+        itemStyle: {
+            areaColor: '#abbd90'
+        },
+        label: {
+            color: '#d68a76',
+            show: false
+        }
+    }
+]
